6c82e0ecd382f88a19a776a533ab31d3cfbc33f8
Loren <neutronstar@gmail.com>
2015-02-14 15:19:21 -0800

    fixed browserify issue

--- gulpfile.js
+++ gulpfile.js
@@ -5,25 +5,32 @@ var gulp        = require('gulp');
 var browserify  = require('browserify');
 var sourcemaps  = require('gulp-sourcemaps');
 var uglify      = require('gulp-uglify');
+var transform   = require('vinyl-transform');
 var source      = require('vinyl-source-stream');
 var streamify   = require('gulp-streamify');
 var to5ify      = require('6to5ify');
+var util        = require('gulp-util');
 
 gulp.task('browserify', function() {
-  var bundleStream = 
-      browserify({ debug: true, paths:['./src/js', './']})
-        .transform(to5ify.configure({
+  var bundle;
+
+  bundle = transform(function(filename) {
+    util.log('bundling ' + filename);
+
+    return browserify( filename, { debug: true, paths:['./client/src/js', './']})
+        .transform( to5ify.configure({
           sourceMapRelative: __dirname,
           ignore: /(^|\/)lib\//
-        }))
-        .require('./src/js/index.js', { entry: true })
-        .bundle();
-
-  bundleStream
-    .pipe(source('app.js'))
+        }) )
+      .bundle()
+    ;
+  });
+  
+  return gulp.src('./client/src/js/entry/*.js')
+    .pipe( bundle )
     //.pipe(streamify(sourcemaps.init( {loadMaps: true} )))
     //.pipe(streamify(uglify()))
     //.pipe(streamify(sourcemaps.write()))
-    .pipe(gulp.dest('./dist/js/'))
+    .pipe( gulp.dest('./dist/js/') )
   ;
 });
--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app     = express();
 
 app.get('/', function(req, res) {
+  console.log(req.queries);
   res.send('<h1>Hello World!</h1>');
 });
 
